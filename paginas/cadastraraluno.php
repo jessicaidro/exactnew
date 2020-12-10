f749fe0b02e84e0c7e10fd5e7fb9e6f276ee5765
jessicaidro <jessicaidro@gmail.com>
2020-12-10 20:51:43 -0300

    User

--- paginas/cadastraraluno.php
+++ paginas/cadastraraluno.php
@@ -1,47 +1,58 @@
 <?php
-	include_once "../index.php";
+	include_once "../z_top.php";
 ?>
 
+			<div class="container">	
+
 				<div class="row">
-					<div class="col s12 m6 push-m3">
 					<h3>Cadastro <b>Aluno</b></h3>
-						<form action="../Forms/formaluno.php" id="formAluno" method="POST">
-							<div class="input-field col s12">
-								
-								<input type="text" name="txtNome" id="txtNome" data-length="60" placeholder="Insira seu nome completo"/>
-								<label for="txtNome">Nome Completo*</label>
-
-								<input type="text" name="txtUsuario" id="txtUsuario" data-length="60" placeholder="Insira seu nome de usuário"/>
-								<label for="txtUsuario">Usuário*</label>
-
-								<input type="text" name="txtSenha" id="txtSenha" data-length="60" placeholder="Digite a senha"/>
-								<label for="txtSenha">Senha*</label>
-
-								<input type="text" name="txtConfirmar" id="txtConfirmar" data-length="60" placeholder="Confirme a senha"/>
-								<label for="txtConfirmar">Confirme a Senha*</label>
-
-								<input type="text" name="txtEmail" id="txtEmail" data-length="60" placeholder="Digite seu e-mail"/>
-								<label for="txtEmail">E-mail*</label>
-
-								<!-- <div class="input-field col s12"> -->
-    								<!-- <select>
-										<//?php
-											include "../BO/cadastrocursoBO.php";
-											$resultaado = buscarCursoBO();
-												if($resultado) {
-													while($r = $resultado->fetch(PDO::FETCH_OBJ)) {
-														echo "<option value='$r=$id_curso'> $r->$nome </option>"; 
-													}
-												}
-										?>
-									</select>	 -->
-									<!-- <label>Selecione o Curso</label> -->
-
-
-								<button name="acao" value="CadastrarUserAluno" class="btn waves-effect waves-light " type="submit"><i class="material-icons right">send</i>CADASTRAR</button>
-							</div>
-						</form>
-					<br>
-					<br>
+					<form action="../Forms/formaluno.php" id="formAluno" method="POST" class="col s12">
+					
+					<div class="input-field col s12">
+						<input type="text" name="txtNome" id="txtNome" data-length="60" placeholder="Insira seu nome completo"/>
+						<label for="txtNome">Nome Completo*</label>
 					</div>
+					
+					<div class="input-field col s12">
+						<input type="text" name="txtUsuario" id="txtUsuario" data-length="60" placeholder="Insira seu nome de usuário"/>
+						<label for="txtUsuario">Usuário*</label>
+					</div>
+					
+		
+					<div class="input-field col s6">
+						<input type="text" name="txtSenha" id="txtSenha" data-length="60" placeholder="Digite a senha"/>
+						<label for="txtSenha">Senha*</label>
+					</div>
+					
+					<div class="input-field col s6">
+						<input type="text" name="txtConfirmar" id="txtConfirmar" data-length="60" placeholder="Confirme a senha"/>
+						<label for="txtConfirmar">Confirme a Senha*</label>
+					</div>
+					
+					<div class="input-field col s12">
+						<input type="text" name="txtEmail" id="txtEmail" data-length="60" placeholder="Digite seu e-mail"/>
+						<label for="txtEmail">E-mail*</label>
+					</div>
+
+					<div class="input-field col s12"> 
+    					<select>
+							<option value="" disabled selected>Escolha uma opção</option>
+							<option value="1" id="txtCurso">Nome Curso</option>
+						</select>
+						<label for="txtCurso"> Selecione o Curso*</label>
+					</div>
+					<div class="input-field col s12"> 
+						<br>
+						<br>
+						<button name="acao" value="CadastrarUserAluno" 
+							class="btn waves-effect waves-light" type="submit"><i class="material-icons right">send</i>Cadastrar</button>
+					</div>
+					</form>
 				</div>
+			</div>
+
+				<script>
+					$(document).ready(function(){
+    					$('select').formSelect();
+  					});
+				</script>
