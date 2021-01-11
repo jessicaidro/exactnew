4eb8dabf585d80cbcda2ff9a8dd507e5c39c19f1
jessicaidro <jessicaidro@gmail.com>
2021-01-11 17:57:06 -0300

    Alteração na tela inicial

--- index.php
+++ index.php
@@ -2,120 +2,164 @@
   include_once "z_top.php";
 ?>
 
-  <div id="index-banner" class="parallax-container">
-    <div class="section no-pad-bot">
-      <div class="container">
-        <br><br>
-        <h1 class="header center blue-text text-darken-4">E X A C T</h1>
-        <div class="row center">
-          <h5 class="header col s12 light blue-text text-darken-4">Biblioteca Online de TCC e Artigos Científicos</h5>
-        </div>
-        <br><br>
-
-      </div>
+<div id="index-banner" class="parallax-container">
+  <div class="section no-pad-bot">
+   <div class="container">
+     <br><br>
+      <h1 class="header center blue-text text-darken-4">E X A C T</h1>
+    <div class="row center">
+      <h5 class="header col s12 light blue-text text-darken-4">Biblioteca Online de TCC e Artigos Científicos</h5>
     </div>
-    <div class="parallax"><img src="img/slide01.png" alt="Unsplashed background img 1"></div>
+    <br><br>
+    </div>
   </div>
-  <div class="container">
+  <div class="parallax"><img src="img/slide01.png" alt="Unsplashed background img 1">
+  </div>
+
+</div>
+
+<div class="container">
     <div class="section">
 
-      <!--   Intruções section   -->
+<!--   Intruções section   -->
       <div class="row">
         <div class="col s12 m4">
           <div class="icon-block">
             <h2 class="center blue-text text-darken-4"><i class="material-icons">plus_one</i></h2>
             <h5 class="center">Realize seu cadastro</h5>
-
             <p class="light">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
           </div>
         </div>
 
-        <div class="col s12 m4">
-          <div class="icon-block">
-            <h2 class="center blue-text text-darken-4"><i class="material-icons">insert_drive_file</i></h2>
-            <h5 class="center">Anexe seu trabalho</h5>
-
-            <p class="light">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
-          </div>
+      <div class="col s12 m4">
+        <div class="icon-block">
+          <h2 class="center blue-text text-darken-4"><i class="material-icons">insert_drive_file</i></h2>
+          <h5 class="center">Anexe seu trabalho</h5>
+          <p class="light">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
         </div>
+      </div>
 
-        <div class="col s12 m4">
-          <div class="icon-block">
-            <h2 class="center blue-text text-darken-4"><i class="material-icons">pageview</i></h2>
-            <h5 class="center">E todos poderão visualizar</h5>
-
-            <p class="light">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>
-          </div>
+      <div class="col s12 m4">
+        <div class="icon-block">
+          <h2 class="center blue-text text-darken-4"><i class="material-icons">pageview</i></h2>
+          <h5 class="center">E todos poderão visualizar</h5>
+          <p class="light">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>
         </div>
       </div>
 
     </div>
+    </div>
   </div>
-
-      <!--   Trabalhos section   -->
-<div class="parallax-container valign-wrapper">
+  
+<!--   Trabalhos section   -->
+  <div class="parallax-container valign-wrapper">
     <div class="container">
-      <div class="row center">
-          <h5 class="header col s12 light brown-text text-darken-1">De olho nas novidades! <div class="chip">NOVO<i class="tiny material-icons amber-text text-darken-3">star</i></div></h5>
-      </div>
+      <div class="row">
+        <h5 class="header col s12 brown-text text-darken-1">De olho nas novidades! <div class="chip v-middle">NOVO<i class="tiny material-icons amber-text text-darken-3 v-middle">star</i></div></h5>
+        <div class="divider"></div>
+        </div>
+
         <div class="row">
           <div class="col s12 m6 l3">
-            <div class="card">
+            <div class="card cardbox">
               <div class="card-content">
-                <h5 class="brown-text text-darken-1">Título do Trabalho aqui</h5>
-                <p class="black-text">Um descrição bem breve do trabalho aqui.</p>
-              </div>
-              <div class="card-content grey lighten-4 brown-text text-darken-1">
-                <div id="test4">Leia Mais</div>
+                <h5 class="blue-text text-darken-1">Título do Trabalho aqui</h5>
+                <p class="black-text">Um descrição bem breve do trabalho aqui Um descrição bem breve do trabalho aqui.</p> <br>
+                <a class="waves-effect waves-light btn cardbox">Ver Trabalho</a>
               </div>
             </div>
           </div>
 
-        </div>
-    </div>
-  </div>
+          <div class="col s12 m6 l3">
+            <div class="card cardbox">
+              <div class="card-content">
+                <h5 class="blue-text text-darken-1">Título do Trabalho aqui</h5>
+                <p class="black-text">Um descrição bem breve do trabalho aqui Um descrição bem breve do trabalho aqui.</p> <br>
+                <a class="waves-effect waves-light btn cardbox">Ver Trabalho</a>
+              </div>
+            </div>
+          </div>
 
+          <div class="col s12 m6 l3">
+            <div class="card cardbox">
+              <div class="card-content">
+                <h5 class="blue-text text-darken-1">Título do Trabalho aqui</h5>
+                <p class="black-text">Um descrição bem breve do trabalho aqui Um descrição bem breve do trabalho aqui.</p> <br>
+                <a class="waves-effect waves-light btn cardbox">Ver Trabalho</a>
+              </div>
+            </div>
+          </div>
 
-      <!--   Frase  section   -->
-   <div class="parallax-container valign-wrapper">
-    <div class="section no-pad-bot">
-      <div class="container">
-        <div class="row center">
-          <h5 class="header col s12 light">“[…] busquem conhecimento. “ — BILÚ, et. 2009</h5>
+          <div class="col s12 m6 l3">
+            <div class="card cardbox">
+              <div class="card-content">
+                <h5 class="blue-text text-darken-1">Título do Trabalho aqui</h5>
+                <p class="black-text">Um descrição bem breve do trabalho aqui Um descrição bem breve do trabalho aqui.</p> <br>
+                <a class="waves-effect waves-light btn cardbox">Ver Trabalho</a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-    <div class="parallax"><img src="img/background2.jpg" alt="Unsplashed background img 2"></div>
   </div>
 
-  <div class="container">
-    <div class="section">
-
-      <div class="row">
-        <div class="col s12 center">
-          <h3><i class="mdi-content-send brown-text"></i></h3>
-          <h4>Sobre o IFSP-BTV</h4>
-          </div>
-          <div class="col s6">
-            <iframe width="560" height="315"  src="https://www.youtube.com/embed/RFxjW4ki6fU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          </div>
-          <div class="col s6"> 
-            <p class="left-align light">O prédio do CETEB contém 3.662,19 m² de área construída assim distribuídos: 7 salas de aula, 16 laboratórios e 15 ambientes administrativos. No primeiro semestre de 2009, começaram as primeiras tratativas para a realização do acordo de cooperação entre a Prefeitura Municipal de Boituva, a Associação Profissionalizante “Vereador Jandir Schincariol” e o Instituto Federal de Educação, Ciência e Tecnologia de São Paulo.</p> 
-          </div>
-      </div>
-    </div>
-  </div>
-
+  <!--   Todos os Trabalhos section   -->
   <div class="parallax-container valign-wrapper">
-    <div class="section no-pad-bot">
-      <div class="container">
-        <div class="row center">
-          <h5 class="header col s12 light">Clique aqui e saiba mais!</h5>
+    <div class="container">
+      <div class="row">
+        <h5 class="header col s12 brown-text text-darken-1">Todos os Trabalhos!</h5>
+        <div class="divider"></div>
+        </div>
+
+        <div class="row">
+          <div class="col s12 m6 l3">
+            <div class="card cardbox">
+              <div class="card-content">
+                <h5 class="blue-text text-darken-1">Título do Trabalho aqui</h5>
+                <p class="black-text">Um descrição bem breve do trabalho aqui Um descrição bem breve do trabalho aqui.</p> <br>
+                <a class="waves-effect waves-light btn cardbox">Ver Trabalho</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col s12 m6 l3">
+            <div class="card cardbox">
+              <div class="card-content">
+                <h5 class="blue-text text-darken-1">Título do Trabalho aqui</h5>
+                <p class="black-text">Um descrição bem breve do trabalho aqui Um descrição bem breve do trabalho aqui.</p> <br>
+                <a class="waves-effect waves-light btn cardbox">Ver Trabalho</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col s12 m6 l3">
+            <div class="card cardbox">
+              <div class="card-content">
+                <h5 class="blue-text text-darken-1">Título do Trabalho aqui</h5>
+                <p class="black-text">Um descrição bem breve do trabalho aqui Um descrição bem breve do trabalho aqui.</p> <br>
+                <a class="waves-effect waves-light btn cardbox">Ver Trabalho</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col s12 m6 l3">
+            <div class="card cardbox">
+              <div class="card-content">
+                <h5 class="blue-text text-darken-1">Título do Trabalho aqui</h5>
+                <p class="black-text">Um descrição bem breve do trabalho aqui Um descrição bem breve do trabalho aqui.</p> <br>
+                <a class="waves-effect waves-light btn cardbox">Ver Trabalho</a>
+              </div>
+            </div>
+          </div>
+
+          <a class="waves-effect waves-light btn cardbox center">Ver todos os trabalhos</a>
+
         </div>
       </div>
     </div>
-    <div class="parallax"><img src="img/background3.jpg" alt="Unsplashed background img 3"></div>
   </div>
+
 
   <?php
   include_once "z_footer.php";
--- z_top.php
+++ z_top.php
@@ -9,16 +9,14 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <!-- <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/> -->
-
-  <!-- <link href="../css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-  <link href="../css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/> -->
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
   <script src="js/materialize.min.js"></script>
   <script src="../js/materialize.min.js"></script>
+
+  <link rel="stylesheet" href="style.css">
+
 </head>
 <body>
   <nav class="blue-grey lighten-5">
